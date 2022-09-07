2302396aa4f02d18c13d2df07c4d8b6aa555a3e4
ggazitbyul <ngusy123@gmail.com>
2022-09-07 11:00:05 +0900

    Println 추가

--- src/academy/pocu/HelloPocu.java
+++ src/academy/pocu/HelloPocu.java
@@ -3,5 +3,8 @@ package academy.pocu;
 public class HelloPocu {
     public static void main(String[] args) {
         System.out.println("Hello POCU");
+        System.out.println(12345);
+        System.out.println(3.14);
+        System.out.println("Your score is" + 100);
     }
 }
