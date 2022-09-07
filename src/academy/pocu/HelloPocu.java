9524375bbe287fbdf20eb26458df911a6e265f89
ggazitbyul <ngusy123@gmail.com>
2022-09-07 11:09:58 +0900

    "Printf \ >%n"

--- src/academy/pocu/HelloPocu.java
+++ src/academy/pocu/HelloPocu.java
@@ -4,7 +4,7 @@ public class HelloPocu {
     public static void main(String[] args) {
         String name = "Mumu";
         int score = 64;
-        System.out.printf("%s's score: %d\n", name, score);
+        System.out.printf("%s's score: %d%n", name, score);
 //      %s는 문자, %d는 정수
     }
 }
