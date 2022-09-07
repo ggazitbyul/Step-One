79686aa388641283f2fb8bb1060f25e96f9671df
ggazitbyul <ngusy123@gmail.com>
2022-09-07 11:07:19 +0900

    Printf

--- src/academy/pocu/HelloPocu.java
+++ src/academy/pocu/HelloPocu.java
@@ -2,9 +2,9 @@ package academy.pocu;
 
 public class HelloPocu {
     public static void main(String[] args) {
-        System.out.println("Hello POCU");
-        System.out.println(12345);
-        System.out.println(3.14);
-        System.out.println("Your score is" + 100);
+        String name = "Mumu";
+        int score = 64;
+        System.out.printf("%s's score: %d\n", name, score);
+//      %s는 문자, %d는 정수
     }
 }
