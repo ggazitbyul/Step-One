c9e5856c9f99dd0bd520cf8ab7f25d86346ea5d6
ggazitbyul <ngusy123@gmail.com>
2022-10-13 16:22:04 +0900

    Clock 구조 변경. seconds 하나에 더하거나 빼고 시,분,초 구분하여 표현

--- src/Clock/Clock.java
+++ src/Clock/Clock.java
@@ -1,71 +1,35 @@
 package Clock;
 
 public class Clock {
-    private byte hours = 10;
-    private byte minutes = 0;
-    private byte seconds = 0;
+    private int seconds = 0;
 
-    public Clock() {
-
+    public Clock(int seconds) {
+        addSeconds(seconds);
     }
 
     public byte getHours() {
-        return this.hours;
-    }
-
-    public void setHours(byte hours) {
-        int value = hours - 1;
-
-        while (value < 0) {
-            value += 12; // 0이거나 작은수일 경우만  while 동작. 12를 더한다.
-        }
-
-        this.hours = (byte) (value % 12 + 1);
+        int hours = this.seconds / 60 / 60;
+        return hours == 0 ? 12 : (byte) hours;
     }
 
     public byte getMinutes() {
-        return this.minutes;
-    }
-
-    public void setMinutes(byte minutes) {
-        int wrapCount = 0;
-
-        while (minutes < 0) {
-            --wrapCount; // 마이너스경우
-            minutes += 60;
-        }
-        wrapCount += minutes / 60;  // 플러스 경우
-
-        this.minutes = (byte) (minutes % 60);
-
-        if (wrapCount != 0) {
-            setHours((byte) (this.hours + wrapCount));
-        }
+        return (byte) (this.seconds / 60 % 60);
     }
 
     public byte getSeconds() {
-        return this.seconds;
+        return (byte) (this.seconds % 60);
     }
 
-    public void setSeconds(byte seconds) {
-        int wrapCount = 0;
+    public void addSeconds(int seconds) {
+        final int HALF_DAY_IN_SECONDS = 60 * 60 * 12;
 
-        while (seconds < 0) {
-            --wrapCount;
-            seconds += 60;
+        int value = this.seconds + seconds;
+        while (value < 0) {
+            value += HALF_DAY_IN_SECONDS;
+
         }
-
-        wrapCount += seconds / 60;
-
-        this.seconds = (byte) (seconds % 60);
-        if(wrapCount!=0){
-            setMinutes((byte) (this.minutes + wrapCount));
-        }
+        this.seconds = value % HALF_DAY_IN_SECONDS;
     }
 
-    public void setTime(byte hours, byte minutes, byte seconds) {
-        setHours(hours);
-        setMinutes(minutes);
-        setSeconds(seconds);
-    }
+
 }
--- src/Clock/app/Program.java
+++ src/Clock/app/Program.java
@@ -7,23 +7,12 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 //   시,분,초 입력 받고 정제된 값 그대로 출력까지.
-        Clock test = new Clock();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("'시'를 입력하세요 ");
-        byte hour = scanner.nextByte();
+        System.out.println("더하거나 뺄'초'를 입력하세요 ");
+        int seconds = scanner.nextInt();
 
-        System.out.println("'분'를 입력하세요 ");
-        byte minute = scanner.nextByte();
-
-        System.out.println("'초'를 입력하세요 ");
-        byte second = scanner.nextByte();
-
-        test.setTime(hour,minute,second);
-//        test.setHours(hour);
-//        test.setMinutes(minute);
-//        test.setSeconds(second);
-
+        Clock test = new Clock(seconds);
         System.out.printf("%d시, %d분, %d초 입니다. %s",test.getHours(),test.getMinutes(),test.getSeconds(),System.lineSeparator());
 
     }
