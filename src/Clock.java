f3f0ede9418a8ce7219f5dba3528ffdda7ef1b9a
ggazitbyul <ngusy123@gmail.com>
2022-10-13 13:19:28 +0900

    Clock() 프로토타입 코딩

--- src/Clock.java
+++ src/Clock.java
@@ -1,2 +1,39 @@
 public class Clock {
+    private byte hours = 10;
+    private byte minutes = 0;
+    private byte seconds = 0;
+
+    public Clock() {
+
+    }
+
+    public byte getHours() {
+        return this.hours;
+    }
+
+    public void setHours(byte hours) {
+        this.hours = hours;
+    }
+
+    public byte getMinutes() {
+        return this.minutes;
+    }
+
+    public void setMinutes(byte minutes) {
+        this.minutes = minutes;
+    }
+
+    public byte getSeconds() {
+        return this.seconds;
+    }
+
+    public void setSeconds(byte seconds) {
+        this.seconds = seconds;
+    }
+
+    public void setTime(byte hours, byte minutes, byte seconds) {
+        setHours(hours);
+        setMinutes(minutes);
+        setSeconds(seconds);
+    }
 }
